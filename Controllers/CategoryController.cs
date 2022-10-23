04aa580e45ce7a28638a31a7013567b0d1dd7eba
slobodannikolich <cobii.nikolic99@hmail.com>
2022-10-23 12:09:12 +0200

    Added CategoryController with CRUD operations and Views

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoGallery.Models;
+using PhotoGallery.Repositories.Interfaces;
+
+namespace PhotoGallery.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryRepository.GetCategories();
+            if (categories == null) return NotFound();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+            _categoryRepository.Add(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var category = await _categoryRepository.GetCategory(id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return NotFound();
+            }
+            _categoryRepository.Update(category);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var category = await _categoryRepository.GetCategory(id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var category = await _categoryRepository.GetCategory(id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteItem(Guid id)
+        {
+            var category = await _categoryRepository.GetCategory(id);
+            if (category == null) return NotFound();
+            _categoryRepository.Delete(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
